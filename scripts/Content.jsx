dbfe063fada491adfa082fb9c60b2e65ca2aa80f
Jordan Sefah <js986@njit.edu>
2020-11-08 19:07:02 +0000

    implement oauth for kroger store

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 
 import { Button } from './Button';
 import { Socket } from './Socket';
+import { KrogerLogin } from './KrogerLogin';
 
 export function Content() {
     const [addresses, setAddresses] = React.useState([]);
@@ -25,6 +26,7 @@ export function Content() {
             <br/>
             <input id="recipe_input" placeholder="Search for recipes here by ingredient, meal-type, cuisine, difficulty"></input>
             <button>SEARCH</button>
+            <KrogerLogin/>
         </div>
     );
 }
