20cfde0746eb26ddb48556c50ab4aec5d094abeb
Jordan Sefah <js986@njit.edu>
2020-11-09 02:20:22 +0000

    create recipe jsx file

---
+++ scripts/CommentList.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Header, Divider, Button, Form, Comment } from 'semantic-ui-react';
+
+export default function CommentList( {comments} ) {
+    
+    const commentList = comments.map((comment) => (
+        <Comment>
+            <Comment.Avatar src={comment.picture} alt=""/>
+            <Comment.Content>
+                <Comment.Author as="a">{comment.username}</Comment.Author>
+                <Comment.Metadata>
+                    <div>comment.created_at</div>
+                </Comment.Metadata>
+                <Comment.Text>
+                    {comment.text}
+                </Comment.Text>
+                <Comment.Actions>
+                    <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+            </Comment.Content>
+        </Comment>
+        
+    ));
+    
+    return (
+        <Comment.Group>
+            <Header as="h3" dividing>
+                Comments
+            </Header>
+            {commentList}
+        </Comment.Group>
+        );
+    
+}
---
+++ scripts/Recipe.jsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Container, Header, Divider, Rating, Button, Icon, Image, List, Label } from 'semantic-ui-react';
+import { CommentList } from './CommentList'
+
+
+export default function Recipe({ username, picture, title, id, ingredients, instructions, tags, readyInMinutes, servings, sourceUrl, description, comments, rating}) {
+    
+    const ingredientList = ingredients.map((ingredient, index) => (
+        <List.Item>{ingredient}</List.Item>
+    ));
+    
+    const instructionsList = instructions.map((instruction, index) => (
+        <List.Item>{instruction}</List.Item>
+    ));
+    
+    const tagList = tags.map((tag) => (
+        <Label>{tag}</Label>
+    ));
+    
+    return (
+        <Container>
+            <Header as="h1">{title}</Header>
+            <Header size="medium">By : {username}</Header>
+            <Image src={picture} size="large" rounded/>
+            <Rating maxRating={5} clearable />
+            <Icon name="share" />
+            <Icon name="bookmark" />
+            <Divider/>
+            <Header sub>Difficulty: </Header>
+            <Header as="h3">Description</Header>
+            <p>{description}</p>
+            <Header as="h3">Ingredients</Header>
+            <List>
+                {ingredientList}
+            </List>
+            <Header as="h3">Instructions</Header>
+            <List>
+                {instructionsList}
+            </List>
+            <Header as="h3">Tags</Header>
+            <div className="tags">
+                {tagList}
+            </div>
+            <CommentList comments={comments} />
+            
+        </Container>
+        );
+}
