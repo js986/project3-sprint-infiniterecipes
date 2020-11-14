8edcfe48bfa4e49e91016287b9daa38ddd2aec01
Hasan Dulgeroglu <hbd3@njit.edu>
2020-11-14 01:37:03 +0000

    Add tests for get_user_id and get_recipe.

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -17,8 +17,10 @@ class MyTest(TestCase):
     
     SQLALCHEMY_DATABASE_URI = "sqlite://"
     
+    TEST_ID = 738270100
+    
     TEST_RECIPE = {
-            'user': 738270100,
+            'user': TEST_ID,
             'images': ['https://spoonacular.com/recipeImages/657178-556x370.jpg'],
             'title': 'Protein Packed Carrot Muffins',
             'readyInMinutes': 45,
@@ -27,7 +29,7 @@ class MyTest(TestCase):
             'description': 'A description', 
             'tags': ['gluten free', 'dinner'],
             'ingredients': [{'name': 'Spice Rub', 'amount': 1.0, 'unit': 'tbsp'}],
-            'instructions': [{'number': 1, 'step': 'Preheat oven to 350 f.Grease a muffin tin or line it with cups.'}]
+            'instructions': [{'number': 1, 'step': 'Preheat oven to 350 f.'}]
         }
         
     TEST_USER = {
@@ -53,12 +55,55 @@ class MyTest(TestCase):
     
     def test_add_user(self):
         user_id = db_queries.add_user(self.TEST_USER)
-        self.assertEqual(self.TEST_USER['name'], db.session.query(models.Users).get(user_id).name)
-        self.assertEqual(self.TEST_USER['email'], db.session.query(models.Users).get(user_id).email)
-        self.assertEqual(self.TEST_USER['imageURL'], db.session.query(models.Users).get(user_id).profile_pic)
+        self.assertEqual(self.TEST_USER['name'], 
+                        db.session.query(models.Users).get(user_id).name)
+        self.assertEqual(self.TEST_USER['email'], 
+                        db.session.query(models.Users).get(user_id).email)
+        self.assertEqual(self.TEST_USER['imageURL'], 
+                        db.session.query(models.Users).get(user_id).profile_pic)
     
     def test_add_recipe(self):
-        db.session.add(models.Users(id=738270100, ))
+        db.session.add(models.Users(id=self.TEST_ID, 
+        email=self.TEST_USER['email'], 
+        name=self.TEST_USER['name'],
+        shopping_list = [], 
+        shared_recipes=[], 
+        saved_recipes=[], 
+        profile_pic = self.TEST_USER['imageURL']))
+        
+        recipe_id = db_queries.add_recipe(self.TEST_RECIPE)
+        
+        self.assertEqual(self.TEST_RECIPE['user'], 
+                        db.session.query(models.Recipe).get(recipe_id).user_id)
+        self.assertEqual(self.TEST_RECIPE['title'], 
+                        db.session.query(models.Recipe).get(recipe_id).title)
+        self.assertEqual(self.TEST_RECIPE['description'], 
+                        db.session.query(models.Recipe).get(recipe_id).description)
+        self.assertEqual(self.TEST_RECIPE['images'], 
+                        db.session.query(models.Recipe).get(recipe_id).images)
+        self.assertEqual(self.TEST_RECIPE['difficulty'], 
+                        db.session.query(models.Recipe).get(recipe_id).difficulty)
+        self.assertEqual(self.TEST_RECIPE['ingredients'], 
+                        db.session.query(models.Recipe).get(recipe_id).ingredients)
+        self.assertEqual(self.TEST_RECIPE['instructions'], 
+                        db.session.query(models.Recipe).get(recipe_id).instructions)
+        self.assertEqual(self.TEST_RECIPE['readyInMinutes'], 
+                        db.session.query(models.Recipe).get(recipe_id).ready_in_minutes)
+        self.assertEqual(self.TEST_RECIPE['servings'], 
+                        db.session.query(models.Recipe).get(recipe_id).servings)
+    
+    def test_get_user_id(self):
+        db.session.add(models.Users(id=self.TEST_ID, 
+        email=self.TEST_USER['email'], 
+        name=self.TEST_USER['name'],
+        shopping_list = [], 
+        shared_recipes=[], 
+        saved_recipes=[], 
+        profile_pic = self.TEST_USER['imageURL']))
+        
+        user_id = db_queries.get_user_id('tester@tester.com')
+        self.assertEqual(user_id, self.TEST_ID)
+
         
 if __name__ == "__main__":
     unittest.main()
