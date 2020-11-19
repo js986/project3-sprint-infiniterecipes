ab5bb82e5cc0f56aad652645d90785a1a6108682
Jordan Sefah <js986@njit.edu>
2020-11-19 04:28:34 +0000

    add saving functionality to recipes

--- scripts/Recipe.jsx
+++ scripts/Recipe.jsx
@@ -1,114 +1,140 @@
+/* eslint-disable import/no-cycle, react/no-array-index-key */
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { Container, Header, Divider, Rating, Button, Icon, Image, List, Label } from 'semantic-ui-react';
+import {
+  Container, Header, Divider, Rating, Button, Icon, Image, List, Label,
+} from 'semantic-ui-react';
+import ReactHtmlParser from 'react-html-parser';
 import { Socket } from './Socket';
 import { Content } from './Content';
-import ReactHtmlParser from 'react-html-parser';
 import { User } from './User';
 
-export function Recipe({ id }) {
-    const [recipe,setRecipe] = React.useState({});
-    const [ingredients,setIngredients] = React.useState([]);
-    const [instructions,setInstructions] = React.useState([]);
-    const [tags,setTags] = React.useState([]);
-    
-    const ingredientList = ingredients.map((ingredient, index) => (
-        <List.Item key={index} >{ingredient["amount"] + " " + ingredient["unit"] + " " + ingredient["name"]}</List.Item>
-    ));
-    
-    const instructionsList = instructions.map((instruction, index) => (
-        <List.Item key={index}>{instruction["step"]}</List.Item>
-    ));
-    
-    const tagList = tags.map((tag, index) => (
-        <Label key={index} >{tag}</Label>
-    ));
-    
-    
-    function getRecipeData() {
-        React.useEffect(() => {
-            Socket.on('recipe page load', (data) => {
-                console.log('Received recipe from the server: ' + data["recipe"])
-                setRecipe(data['recipe'])
-                setIngredients(data['recipe']['ingredients'])
-                setInstructions(data['recipe']['instructions'])
-                setTags(data['recipe']['tags'])
-            })
-        });
-    }
-    
-    function getCartNumItems(){
-        React.useEffect(() => {
-            Socket.on('received cart item num', (data) => {
-               localStorage.setItem('cartNumItems',data['cart_num']); 
-            })
-        });
-    }
+export function Recipe() {
+  const [recipe, setRecipe] = React.useState({});
+  const [ingredients, setIngredients] = React.useState([]);
+  const [instructions, setInstructions] = React.useState([]);
+  const [tags, setTags] = React.useState([]);
 
-    function handleSubmit(user){
-        Socket.emit('user page', {
-            'user_id' : user
-        });
-        ReactDOM.render(<User />, document.getElementById('content'));
-    
+  const ingredientList = ingredients.map((ingredient, index) => (
+    <List.Item key={index}>{`${ingredient.amount} ${ingredient.unit} ${ingredient.name}`}</List.Item>
+  ));
+
+  const instructionsList = instructions.map((instruction, index) => (
+    <List.Item key={index}>{instruction.step}</List.Item>
+  ));
+
+  const tagList = tags.map((tag, index) => (
+    <Label key={index}>{tag}</Label>
+  ));
+
+  function getRecipeData() {
+    React.useEffect(() => {
+      Socket.on('recipe page load', (data) => {
+        setRecipe(data.recipe);
+        setIngredients(data.recipe.ingredients);
+        setInstructions(data.recipe.instructions);
+        setTags(data.recipe.tags);
+      });
+    });
+  }
+
+  function getCartNumItems() {
+    React.useEffect(() => {
+      Socket.on('received cart item num', (data) => {
+        localStorage.setItem('cartNumItems', data.cart_num);
+      });
+    });
+  }
+
+  function handleSubmit(user) {
+    Socket.emit('user page', {
+      user_id: user,
+    });
+    ReactDOM.render(<User />, document.getElementById('content'));
+  }
+
+  function saveRecipe() {
+    let email = '';
+    if (localStorage.getItem('user_email') !== null) {
+      email = localStorage.getItem('user_email');
     }
-    
-    function addToCart(recipes) {
-        let email = ''
-        if (localStorage.getItem('user_email') !== null){
-            email = localStorage.getItem('user_email');
-        }
-        Socket.emit('add to cart', {
-            'cartItems': recipes,
-            'user_email': email,
-        });
+    Socket.emit('save recipe', {
+      recipe_id: recipe.id,
+      user_email: email,
+    });
+  }
+
+  function addToCart(recipes) {
+    let email = '';
+    if (localStorage.getItem('user_email') !== null) {
+      email = localStorage.getItem('user_email');
     }
-    
-    function goToHomePage(){
-        Socket.emit('content page', {
-            'content page' : 'content page'
-        });
-        ReactDOM.render(<Content />, document.getElementById('content'));
-    }
-    
-    
-    getRecipeData();
-    getCartNumItems();
-    return (
-        <Container>
-            <Button icon labelPosition="left" onClick={goToHomePage}>
-                <Icon name="left arrow" />
-                Back to Homepage
-            </Button>
-            <Divider/>
-            <Header as="h1">{recipe["title"]}</Header>
-            <Header size="medium">By : <Button onClick={() => handleSubmit(recipe["user"])}>{recipe["name"]}</Button></Header>
-            <Image src={recipe["images"]} size="large" bordered/>
-            <Rating maxRating={5} clearable />
-            <Icon name="share" />
-            <Icon name="bookmark" />
-            <Divider/>
-            <Header sub>Difficulty: {recipe["difficulty"]}</Header>
-            <Header sub>Servings: {recipe["servings"]}</Header>
-            <Header sub>Time: {recipe["readyInMinutes"]} Min</Header>
-            <Header as="h3">Description</Header>
-            <p>
-            {ReactHtmlParser(recipe["description"])}
-            </p>
-            <Header as="h3">Ingredients</Header>
-            <List celled>
-                {ingredientList}
-            </List>
-            <Button onClick={() => addToCart(recipe["ingredients"])}>Add Ingredients to Cart</Button>
-            <Header as="h3">Instructions</Header>
-            <List ordered>
-                {instructionsList}
-            </List>
-            <Header as="h3">Tags</Header>
-            <div className="tags">
-                {tagList}
-            </div>
-            
-        </Container>
-        );
+    Socket.emit('add to cart', {
+      cartItems: recipes,
+      user_email: email,
+    });
+  }
+
+  function goToHomePage() {
+    Socket.emit('content page', {
+      'content page': 'content page',
+    });
+    ReactDOM.render(<Content />, document.getElementById('content'));
+  }
+
+  getRecipeData();
+  getCartNumItems();
+  return (
+    <Container>
+      <Button icon labelPosition="left" onClick={goToHomePage}>
+        <Icon name="left arrow" />
+        Back to Homepage
+      </Button>
+      <Divider />
+      <Header as="h1">{recipe.title}</Header>
+      <Header size="medium">
+        By :
+        <Button onClick={() => handleSubmit(recipe.user)}>{recipe.name}</Button>
+      </Header>
+      <Image src={recipe.images} size="large" bordered />
+      <Rating maxRating={5} clearable />
+      <Button.Group basic size="small">
+        <Button icon="share" />
+        <Button icon="bookmark" onClick={saveRecipe} />
+      </Button.Group>
+      <Divider />
+      <Header sub>
+        Difficulty:
+        {recipe.difficulty}
+      </Header>
+      <Header sub>
+        Servings:
+        {recipe.servings}
+      </Header>
+      <Header sub>
+        Time:
+        {recipe.readyInMinutes}
+        {' '}
+        Min
+      </Header>
+      <Header as="h3">Description</Header>
+      <p>
+        {ReactHtmlParser(recipe.description)}
+      </p>
+      <Header as="h3">Ingredients</Header>
+      <List celled>
+        {ingredientList}
+      </List>
+      <Button onClick={() => addToCart(recipe.ingredients)}>Add Ingredients to Cart</Button>
+      <Header as="h3">Instructions</Header>
+      <List ordered>
+        {instructionsList}
+      </List>
+      <Header as="h3">Tags</Header>
+      <div className="tags">
+        {tagList}
+      </div>
+
+    </Container>
+  );
 }
--- tests/socket_tests.py
+++ tests/socket_tests.py
@@ -1,7 +1,8 @@
 import os
 import sys
-import unittest  
-import unittest.mock as mock  
+import unittest
+import unittest.mock as mock
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import app
 from app import socketio, app, emit_all_recipes
@@ -47,8 +48,21 @@ TEST_ADD_RECIPE = {
 
 TEST_ADD_TAG = {"name": "tag"}
 
+TEST_SAVE_RECIPE = {
+    "id": TEST_RECIPE_ID,
+    "user_id": TEST_ID,
+    "title": "Protein Packed Carrot Muffins",
+    "description": "A description",
+    "difficulty": DIFFICULTY,
+    "instructions": [{"number": 1, "step": "Preheat oven to 350 f."}],
+    "ready_in_minutes": 45,
+    "servings": 6,
+    "images": ["https://spoonacular.com/recipeImages/657178-556x370.jpg"],
+    "ingredients": [{"name": "Spice Rub", "amount": 1.0, "unit": "tbsp"}],
+}
+
+
 class AppTestCase(unittest.TestCase):
-    
     def setUp(self):
         pass
 
@@ -56,27 +70,31 @@ class AppTestCase(unittest.TestCase):
         with mock.patch("app.emit_all_recipes"):
             with mock.patch("db_queries.get_user", mocked_get_user):
                 with mock.patch("db_queries.add_user"):
-                    with mock.patch("db_queries.get_shopping_list", mocked_get_shopping_list):
+                    with mock.patch(
+                        "db_queries.get_shopping_list", mocked_get_shopping_list
+                    ):
                         client = socketio.test_client(app)
-                        client.emit('new google user', {'test':"test"})
+                        client.emit("new google user", {"test": "test"})
                         received = client.get_received()
-                        self.assertEqual(TEST_ADD_USER["name"], received[-1]["args"][0]["username"])
+                        self.assertEqual(
+                            TEST_ADD_USER["name"], received[-1]["args"][0]["username"]
+                        )
                         client.disconnect()
-                        
+
     def test_on_old_google_user(self):
         with mock.patch("app.emit_all_recipes"):
             client = socketio.test_client(app)
-            client.emit('old google user', {})
+            client.emit("old google user", {})
             received = client.get_received()
-            self.assertEqual('logout', received[-1]["args"][0]["logout"])
+            self.assertEqual("logout", received[-1]["args"][0]["logout"])
             client.disconnect()
-        
+
     def test_on_connect(self):
         with mock.patch("app.emit_all_recipes"):
             socketio_test_client = socketio.test_client(app)
             assert socketio_test_client.is_connected()
             received = socketio_test_client.get_received()
-            self.assertEqual('Connected', received[-1]["args"][0]["test"])
+            self.assertEqual("Connected", received[-1]["args"][0]["test"])
             socketio_test_client.disconnect()
 
     def test_on_disconnect(self):
@@ -84,40 +102,61 @@ class AppTestCase(unittest.TestCase):
             client1 = socketio.test_client(app)
             client1.disconnect()
             assert not client1.is_connected()
-    
+
     def test_on_recipe_page(self):
         with mock.patch("app.emit_all_recipes"):
             with mock.patch("db_queries.get_user", mocked_get_user):
                 with mock.patch("db_queries.get_recipe", mocked_get_recipe):
                     client = socketio.test_client(app)
-                    client.emit('recipe page', {'id':1})
+                    client.emit("recipe page", {"id": 1})
                     received = client.get_received()
-                    self.assertEqual(TEST_RECIPE["title"], received[-1]["args"][0]["recipe"]["title"])
+                    self.assertEqual(
+                        TEST_RECIPE["title"], received[-1]["args"][0]["recipe"]["title"]
+                    )
                     client.disconnect
-                    
-        
-#         pass
-#     def test_on_new_search(self):
-#         pass
+
+    #         pass
+    #     def test_on_new_search(self):
+    #         pass
     def test_on_new_user_page(self):
         with mock.patch("app.emit_all_recipes"):
             with mock.patch("db_queries.get_user", mocked_get_user):
                 client = socketio.test_client(app)
-                client.emit('user page', {'user_id':TEST_ID})
+                client.emit("user page", {"user_id": TEST_ID})
                 received = client.get_received()
-                self.assertEqual(TEST_ADD_USER["name"], received[-1]["args"][0]["user"]['name'])
+                self.assertEqual(
+                    TEST_ADD_USER["name"], received[-1]["args"][0]["user"]["name"]
+                )
                 client.disconnect()
-        
-#     def test_add_to_cart(self):
-#         pass
+
+    #     def test_add_to_cart(self):
+    #         pass
     def test_on_new_zip(self):
         with mock.patch("app.emit_all_recipes"):
             client = socketio.test_client(app)
-            client.emit('new zipcode query', {'zip':"12345"})
+            client.emit("new zipcode query", {"zip": "12345"})
             received = client.get_received()
             print(received)
             self.assertEqual("12345", received[-1]["args"][0])
             client.disconnect()
+
+    def test_on_save_recipe(self):
+        with mock.patch("db_queries.get_user_id", mocked_get_user_id):
+            with mock.patch("db_queries.add_saved_recipe", mocked_add_saved_recipe):
+                client = socketio.test_client(app)
+                client.emit(
+                    "save recipe",
+                    {
+                        "user_email": TEST_USER["email"],
+                        "recipe_id": TEST_SAVE_RECIPE["id"],
+                    },
+                )
+                received = client.get_received()
+                print('received data for saved recipe ' + received)
+                self.assertEqual("tester@tester.com", received[-1]["args"][0])
+                client.disconnect()
+
+
 #     def test_cart_page(self):
 #         pass
 #     def test_content_page(self):
@@ -140,16 +179,28 @@ class AppTestCase(unittest.TestCase):
 #         },
 #     )
 # def mocked_add_user(user_dict):
-#     pass 
+#     pass
+
 
 def mocked_get_shopping_list(user_id):
-    return TEST_ADD_USER['shopping_list']
+    return TEST_ADD_USER["shopping_list"]
+
 
 def mocked_get_user(user_id):
     return TEST_ADD_USER
 
+
+def mocked_get_user_id(user_email):
+    return TEST_ID
+
+
+def mocked_add_saved_recipe(recipe_id, user_id):
+    return TEST_SAVE_RECIPE
+
+
 def mocked_get_recipe(recipe_id):
     return TEST_RECIPE
+
+
 if __name__ == "__main__":
     unittest.main()
-        
